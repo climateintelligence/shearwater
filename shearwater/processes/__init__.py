e0640b21700953009b9955803471bf9db70f2f47
Pingu Carsti <ehbrecht@dkrz.de>
2024-06-17 15:11:56 +0200

    fix linting

--- shearwater/processes/__init__.py
+++ shearwater/processes/__init__.py
@@ -1,7 +1,5 @@
-# from .wps_say_hello import SayHello
 from .wps_cyclone import Cyclone
 
 processes = [
-#    SayHello(),
     Cyclone(),
 ]
--- tests/test_wps_caps.py
+++ tests/test_wps_caps.py
@@ -12,5 +12,4 @@ def test_wps_caps():
     )
     assert sorted(names.split()) == [
         "cyclone",
-        # "hello",
     ]
